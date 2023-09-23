20b12f99f7217dc59aaf8c5224f761f9a91e7d72
Stefan Frede <stefan@frede.dev>
2023-09-23 23:41:18 +0200

    feat(setup): render dynamic menus

---
+++ components/SbHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { MenuLinkStoryblok } from "../component-types-sb";
+
+const storyblokApi = useStoryblokApi();
+const { data } = await storyblokApi.get("cdn/stories/config", {
+  version: "draft",
+  resolve_links: "url",
+});
+
+const headerMenu = ref<MenuLinkStoryblok[]>();
+headerMenu.value = data.story.content.header_menu;
+</script>
+
+<template>
+  <header class="w-full h-24 bg-[#f7f6fd]">
+    <div class="container h-full mx-auto flex items-center justify-between">
+      <NuxtLink to="/">
+        <h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok Nuxt</h1>
+      </NuxtLink>
+      <nav v-if="headerMenu">
+        <ul class="flex space-x-8 text-lg font-bold">
+          <li v-for="blok in headerMenu" :key="blok._uid">
+            <NuxtLink
+              :to="`/${blok.link?.cached_url}`"
+              class="hover:text-[#50b0ae]"
+            >
+              {{ blok.link?.story.name }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+nav a.router-link-active {
+  @apply underline underline-offset-4 decoration-4 decoration-[#50b0ae];
+}
+</style>
