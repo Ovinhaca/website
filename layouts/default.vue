63d7bbc21cda9fea9f7e46eba379aaaa818fae45
Stefan Frede <stefan@frede.dev>
2023-09-23 23:41:18 +0200

    feat(setup): render dynamic menus

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <SbHeader />
+    <slot />
+  </main>
+</template>
---
+++ pages/[...slug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { slug } = useRoute().params as { slug: string[] };
+
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join("/") : "home",
+  { version: "draft" },
+);
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
