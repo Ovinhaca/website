73d1f65957a6a8fe6d482668fce4305fa4d92efb
Stefan Frede <stefan@frede.dev>
2023-11-20 23:28:50 +0100

    feat(components): add menu

--- storyblok/Feature.vue
+++ storyblok/Feature.vue
@@ -14,15 +14,15 @@ const props = defineProps<Props>();
     class="w-full bg-neutral-100 rounded-md overflow-hidden"
   >
     <img
-      :src="`${blok.image?.filename}/m/500x0/`"
-      :alt="blok.image?.alt"
+      :src="`${props.blok.image?.filename}/m/500x0/`"
+      :alt="props.blok.image?.alt"
       class="w-full h-48 xl:h-72 object-cover rounded-t-md"
     />
     <div class="px-12 py-6 rounded-b-md">
       <h3
         class="text-2xl text-neutral-800 uppercase mb-4 tracking-wide font-medium"
       >
-        {{ blok.name }}
+        {{ props.blok.name }}
       </h3>
       <p class="prose text-neutral-500 max-w-[60ch]">
         {{ props.blok.copy }}
