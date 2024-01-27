4a8da58cda4998c6bf1512cf93a2fe3f834a4194
Stefan Frede <stefan@frede.dev>
2024-01-27 18:56:26 +0100

    feat(image): add image section

---
+++ storyblok/Image.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { ImageStoryblok } from "../component-types-sb";
+
+type Props = {
+  blok: ImageStoryblok;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <SbSection maxw="5xl">
+    <img
+      :src="`${props.blok.image?.filename}/m/500x0`"
+      :alt="props.blok.background_image?.alt"
+      class="w-full rounded-md h-48 xl:h-96 object-cover shadow-md border-2 border-neutral-200"
+    />
+  </SbSection>
+</template>
