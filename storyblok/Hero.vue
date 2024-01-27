2409c60b7d2f8bee7628dbf5b30a052dcf223f1c
Stefan Frede <stefan@frede.dev>
2024-01-27 13:05:16 +0100

    refactor(hero): open link internal

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -60,7 +60,6 @@ const props = defineProps<Props>();
           <NuxtLink
             v-if="props.blok.booking_link"
             :to="`${props.blok.booking_link?.cached_url}`"
-            :target="`${props.blok.booking_link?.target}`"
             class="rounded-md bg-amber-600 uppercase px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-500"
           >
             {{ props.blok.booking_label }}
